d707a14cad051fe9e5ffd3caab64a108789bfba6
gururajchadaga <guruuraj.chadaga@gmail.com>
2021-09-10 19:15:21 +0530

    Added QuoteForm prompt

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-
+import { Prompt } from 'react-router-dom';
+import { Fragment } from 'react';
 import Card from '../UI/Card';
 import LoadingSpinner from '../UI/LoadingSpinner';
 import classes from './QuoteForm.module.css';
@@ -7,6 +8,7 @@ import classes from './QuoteForm.module.css';
 const QuoteForm = (props) => {
   const [author, setAuthor] = useState('');
   const [text, setText] = useState('');
+  const [isEntering, setIsEntering] = useState(false);
 
   const submitFormHandler = (event) => {
     event.preventDefault();
@@ -24,41 +26,61 @@ const QuoteForm = (props) => {
   };
 
   const textChangeHandler = (event) => {
-    setText(event.target.value);
+    setText(event.target.value.trim());
+  };
+
+  const formFocusHandler = () => {
+    setIsEntering(true);
+  };
+  const formBlurHandler = () => {
+    if (author.trim() === '' && text.trim() === '') {
+      setIsEntering(false);
+    }
   };
 
   return (
-    <Card>
-      <form className={classes.form} onSubmit={submitFormHandler}>
-        {props.isLoading && (
-          <div className={classes.loading}>
-            <LoadingSpinner />
-          </div>
-        )}
+    <Fragment>
+      <Prompt
+        when={isEntering}
+        message={() => 'Are you sure? Your entered data will be lost'}
+      />
+      <Card>
+        <form
+          className={classes.form}
+          onSubmit={submitFormHandler}
+          onFocus={formFocusHandler}
+          onBlur={formBlurHandler}
+        >
+          {props.isLoading && (
+            <div className={classes.loading}>
+              <LoadingSpinner />
+            </div>
+          )}
 
-        <div className={classes.control}>
-          <label htmlFor='author'>Author</label>
-          <input
-            type='text'
-            id='author'
-            value={author}
-            onChange={authorChangeHandler}
-          />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='text'>Text</label>
-          <textarea
-            id='text'
-            rows='5'
-            value={text}
-            onChange={textChangeHandler}
-          ></textarea>
-        </div>
-        <div className={classes.actions}>
-          <button className='btn'>Add Quote</button>
-        </div>
-      </form>
-    </Card>
+          <div className={classes.control}>
+            <label htmlFor='author'>Author</label>
+            <input
+              type='text'
+              id='author'
+              value={author}
+              onChange={authorChangeHandler}
+            />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor='text'>Text</label>
+            <textarea
+              id='text'
+              rows='5'
+              value={text}
+              onChange={textChangeHandler}
+            ></textarea>
+          </div>
+          <div className={classes.actions}>
+            <button className='btn'>Add Quote</button>
+          </div>
+        </form>
+      </Card>
+    </Fragment>
   );
 };
 
