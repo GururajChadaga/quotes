c00115c38062b04ea33a2f9724cbd16ff3a4ef7d
gururajchadaga <guruuraj.chadaga@gmail.com>
2021-09-10 16:30:04 +0530

    Added components: QuoteList, QuoteItem and '/quotes' route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { Route, Switch } from 'react-router-dom';
-import QuoteForm from './components/quotes/QuoteForm';
 import Layout from './components/layout/Layout';
+import AllQuotes from './pages/AllQuotes';
+import NewQuote from './pages/NewQuote';
+
 function App() {
   return (
     <Layout>
       <Switch>
+        <Route path='/quotes'>
+          <AllQuotes />
+        </Route>
         <Route path='/new-quote'>
-          <QuoteForm />
+          <NewQuote />
         </Route>
       </Switch>
     </Layout>
