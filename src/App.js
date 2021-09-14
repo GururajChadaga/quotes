8118599e47bc5bf19e60532294c9e509a194beaf
gururajchadaga <guruuraj.chadaga@gmail.com>
2021-09-14 19:04:56 +0530

    Optimized with lazy loading the pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Switch, Redirect } from 'react-router-dom';
 import Layout from './components/layout/Layout';
-import AllQuotes from './pages/AllQuotes';
-import NewQuote from './pages/NewQuote';
-import QuoteDetail from './pages/QuoteDetail';
 import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, lazy, Suspense } from 'react';
 import { sendData, fetchData } from './store/actions';
+import LoadingSpinner from './components/UI/LoadingSpinner';
+
+const AllQuotes = lazy(() => import('./pages/AllQuotes'));
+const NewQuote = lazy(() => import('./pages/NewQuote'));
+const QuoteDetail = lazy(() => import('./pages/QuoteDetail'));
 
 let isInitial = true;
 
@@ -23,20 +25,28 @@ function App() {
 
   return (
     <Layout>
-      <Switch>
-        <Route path='/' exact>
-          <Redirect to='/quotes' />
-        </Route>
-        <Route path='/quotes' exact>
-          <AllQuotes />
-        </Route>
-        <Route path='/new-quote'>
-          <NewQuote />
-        </Route>
-        <Route path='/quotes/:quoteId'>
-          <QuoteDetail />
-        </Route>
-      </Switch>
+      <Suspense
+        fallback={
+          <div className='centered'>
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path='/' exact>
+            <Redirect to='/quotes' />
+          </Route>
+          <Route path='/quotes' exact>
+            <AllQuotes />
+          </Route>
+          <Route path='/new-quote'>
+            <NewQuote />
+          </Route>
+          <Route path='/quotes/:quoteId'>
+            <QuoteDetail />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
